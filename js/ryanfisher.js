ab4504a75c80d9bd3a38124eee44e44ba2ea26db
Ryan Fisher <ryancfisher@gmail.com>
2013-05-23 22:43:35 -0700

    Add animated cursor prompt

--- js/ryanfisher.js
+++ js/ryanfisher.js
@@ -1,5 +1,16 @@
+function remove_cursor() {
+  $('#prompt').removeClass('cursor');
+  setTimeout(add_cursor, 1000);
+}
+
+function add_cursor() {
+  $('#prompt').addClass('cursor');
+  setTimeout(remove_cursor, 1000);
+}
+
 function initialize_app() {
   console.log("Initializing app");
+  add_cursor();
 };
 
 $(document).ready(initialize_app);
