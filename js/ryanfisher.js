593ddc2c7b2d9fbddfc71189468f011ada60a313
Ryan Fisher <ryancfisher@gmail.com>
2013-05-26 22:12:29 -0700

    Add text input to terminal prompt

--- js/ryanfisher.js
+++ js/ryanfisher.js
@@ -1,16 +1,22 @@
 function remove_cursor() {
   $('#prompt').removeClass('cursor');
-  setTimeout(add_cursor, 1000);
+  setTimeout(add_cursor, 500);
 }
 
 function add_cursor() {
   $('#prompt').addClass('cursor');
-  setTimeout(remove_cursor, 1000);
+  setTimeout(remove_cursor, 600);
+}
+
+function prepend_text(event) {
+  var character = String.fromCharCode(event.keyCode);
+  $('#terminal-input').append(character);
 }
 
 function initialize_app() {
   console.log("Initializing app");
   add_cursor();
+  $(document).on('keypress', prepend_text);
 };
 
 $(document).ready(initialize_app);
