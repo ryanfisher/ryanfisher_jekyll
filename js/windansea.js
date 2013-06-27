fdf0ca8bf2faa104e7b9822c587a59d0a26a2518
Ryan Fisher <ryancfisher@gmail.com>
2013-06-26 21:47:08 -0700

    Add date list with bold to show current image

--- js/windansea.js
+++ js/windansea.js
@@ -13,11 +13,17 @@ image_dates = [
   '2013-05-11',
 ];
 
+function change_showing(index) {
+  $('#image-list li').removeClass('showing');
+  $('#image-list li[data-image="'+index+'"]').addClass('showing');
+}
+
 function fade_image(index) {
   current = $('.windansea-images[data-image="'+index+'"]');
   z = current.css('z-index');
   if (index+1 >= image_dates.length) { index = 0 }
   else { index++ }
+  change_showing(index);
   current.fadeOut(2000, function () {
     current.css('z-index', z-image_dates.length);
     current.show();
@@ -41,8 +47,20 @@ function preload_images() {
  }
 }
 
+function set_list() {
+ for (var i=0; i < image_dates.length; i++) {
+  item = $('<li />', {
+    text: image_dates[i],
+    'data-image': i
+  });
+  $('#image-list').append(item);
+ }
+}
+
 function initialize_app() {
   preload_images();
+  set_list();
+  change_showing(0);
   setTimeout(function() {fade_image(0);}, 1000);
 }
 
